730578e389070b197122bedd5df805d760c7343e
Jordan <jordie.earl@gmail.com>
2024-08-04 17:59:22 +0200

    Update Bookshelf model

--- books/models.py
+++ books/models.py
@@ -137,12 +137,19 @@ class Bookshelf(models.Model):
         ('unread', 'Unread'),
         ('reading', 'Reading'),
     ]
+    STYLE_CHOICES = [
+        ('audiobook', 'Audiobook'),
+        ('kindle', 'Kindle'),
+        ('paper', 'Paper'),
+    ]
     # Relationships
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     # Fields
     status = models.CharField(max_length=7, choices=STATUS_CHOICES, default='unread')
-    notes = models.TextField(max_length=600, blank=True, null=True)
+    style = models.CharField(max_length=10, choices=STYLE_CHOICES, default='paper')
+    notes = models.TextField(max_length=1000, blank=True, null=True)
+    quotes = models.TextField(max_length=1000, blank=True, null=True)
     added_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
