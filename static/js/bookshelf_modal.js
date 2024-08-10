9f7d36d55eef4724ce9c48af767b2735c354f538
Jordan <jordie.earl@gmail.com>
2024-08-10 10:33:05 +0200

    Add JS to get quotes data and handle quotes edit and save buttons

--- static/js/bookshelf_modal.js
+++ static/js/bookshelf_modal.js
@@ -10,14 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
             const genre = this.getAttribute('data-book-genre');
             const status = this.getAttribute('data-book-status');
             const notes = this.getAttribute('data-book-notes');
+            const quotes = this.getAttribute('data-book-quotes');
 
             document.getElementById(`modalBookTitle${slug}`).innerText = title;
             document.getElementById(`modalBookAuthor${slug}`).innerText = author;
             document.getElementById(`modalBookGenre${slug}`).innerText = genre;
             document.getElementById(`modalBookStatus${slug}`).innerText = status;
             document.getElementById(`modalBookNotes${slug}`).value = notes;
-
-            console.log(notes);
+            document.getElementById(`modalBookQuotes${slug}`).value = quotes;
 
             const detailLink = document.getElementById(`BookDetailLink${slug}`);
             detailLink.href = `/books/${slug}/`;
@@ -31,6 +31,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.querySelectorAll('.notes-form').forEach(form => {
+        const editButton = form.querySelector('button[type="button"]');
+        const saveButton = form.querySelector('button[type="submit"]');
+        const textarea = form.querySelector('textarea');
+
+        editButton.addEventListener('click', function() {
+            textarea.removeAttribute('readonly');
+            editButton.classList.add('d-none');
+            saveButton.classList.remove('d-none');
+        });
+    });
+
+    document.querySelectorAll('.quotes-form').forEach(form => {
         const editButton = form.querySelector('button[type="button"]');
         const saveButton = form.querySelector('button[type="submit"]');
         const textarea = form.querySelector('textarea');
